c07fc8c7c1ac78a80b282ae412c848901f6f1235
caped-baldys <119095339+caped-baldys@users.noreply.github.com>
2022-12-30 20:56:43 +0530

    new

---
+++ stack.py
@@ -0,0 +1,19 @@
+#stack implementation using list
+
+stack =[]
+
+#initalizing stack
+for i in range(10):
+    stack.append(i)
+
+print('Initial Stack')
+print(stack)
+
+#pop() funcion o pip
+
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
+print('Final Stack')
+print(stack)
